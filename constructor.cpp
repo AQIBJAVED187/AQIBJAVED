9102040203863f30f59c1492de2037c2647d6cc8
AQIBJAVED187 <aqibjaved7888@gmail.com>
2024-09-21 10:25:50 +0500

    Add files via upload

---
+++ constructor.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class calculator{
+	int n1;
+	char grade;
+	public:
+		
+		calculator(int f1, char f2)
+		{
+			n1 = f1;
+			grade = f2;
+		}
+		
+		void display()
+		{
+			cout<<"the first data member"<<n1<<endl;
+			cout<<"the second data member"<<grade<<endl;
+		}
+};
+
+main(){
+	calculator t1(100, 'a');
+	t1.display();
+	
+}
